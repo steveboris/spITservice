3f6c8b86758b3eaf56fdd1759ce4e971b2b854c0
steveboris <b.steve189@hotmail.com>
2022-05-20 22:52:45 +0200

    FEA: Header has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
@@ -20,6 +24,10 @@ import { UserLayoutComponent } from './layouts/user-layout/user-layout.component
     BrowserAnimationsModule,
     BrowserModule,
     RouterModule,
+    NgbModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     ComponentsModule
   ],
   providers: [],

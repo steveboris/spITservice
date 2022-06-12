9b3bb1539da5471bf204a64db161ae18103ae088
steveboris <b.steve189@hotmail.com>
2022-06-12 06:20:15 +0200

    WIP

--- src/app/layouts/user-layout/user-layout.module.ts
+++ src/app/layouts/user-layout/user-layout.module.ts
@@ -1,24 +1,44 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-import { RouterModule } from "@angular/router";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheckCircle as farCheckCircle
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faGithub,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
 import { ComponentsModule } from 'src/app/components/components.module';
 
 import { UserLayoutRoutes } from './user-layout.routing';
 import { HomeComponent } from 'src/app/pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     RouterModule.forChild(UserLayoutRoutes),
     FormsModule,
     ReactiveFormsModule,
-    ComponentsModule
-  ]
+    ComponentsModule,
+    FontAwesomeModule,
+  ],
 })
-export class UserLayoutModule { }
+export class UserLayoutModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faCheckCircle,
+      farCheckCircle,
+      faGithub,
+      faLinkedin
+    );
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,84 @@
 import { Component, OnInit } from '@angular/core';
 
+interface User {
+  picture: string;
+  name: string;
+  job: string;
+  description: string;
+  portfolio: string;
+  github: string;
+  linkedIn: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public profiles:User[];
+  public user1: User = {
+    picture: 'https://mdbcdn.b-cdn.net/img/new/avatars/2.webp',
+    name: 'Name',
+    job: 'Job',
+    description:
+      'Invidunt lorem justo sanctus clita. Erat lorem labore ea, ' +
+      'justo dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea ' + 
+      'justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est dolor',
+    github: 'Follow me on Github',
+    linkedIn: 'Follow me on LinkedIn',
+    portfolio: 'Über mich'
+  };
 
-  constructor() { }
+  public user2: User = {
+    picture: 'https://mdbcdn.b-cdn.net/img/new/avatars/2.webp',
+    name: 'Name',
+    job: 'Job',
+    description:
+      'Invidunt lorem justo sanctus clita. Erat lorem labore ea, ' +
+      'justo dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea ' + 
+      'justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est dolor',
+    github: 'Follow me on Github',
+    linkedIn: 'Follow me on LinkedIn',
+    portfolio: 'Über mich'
+  };
 
-  ngOnInit(): void {
+  /**What we do */
+  public cards = [
+    {
+      img: 'https://mdbcdn.b-cdn.net/img/new/avatars/2.webp',
+      title: 'Apps & Software Entwicklung',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+      link: '#'
+    },
+    {
+      img: 'https://mdbcdn.b-cdn.net/img/new/avatars/2.webp',
+      title: 'Webentwicklung',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+      link: '#'
+    },
+    {
+      img: 'https://mdbcdn.b-cdn.net/img/new/avatars/2.webp',
+      title: 'Consulting',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+      link: '#'
+    }
+  ]
+
+  /** Why us */
+  public itemlist = [
+    'Cras justo odio',
+    'Dapibus ac facilisis in',
+    'Morbi leo risus',
+    'Dapibus ac facilisis in',
+    'Cras justo odio',
+    'Morbi leo risus'
+  ]
+  constructor() {
+    this.profiles = []; 
   }
 
+  ngOnInit(): void {
+    this.profiles.push(this.user1);
+    this.profiles.push(this.user2);
+  }
 }

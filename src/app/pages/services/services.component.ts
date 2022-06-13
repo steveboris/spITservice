eb1a154dfe861a8d654897a40bb6e9e1db0cff29
steveboris <b.steve189@hotmail.com>
2022-06-13 09:47:17 +0200

    FEA: services page implemented

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  serviceName: string | null = null;
+  isSoftware: boolean = false;
+  isWebsite: boolean = false;
+  isConsulting: boolean = false;
 
-  ngOnInit(): void {
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.activatedroute.paramMap.subscribe(params => this.serviceName = params.get('name'));
   }
 
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.checkServiceName();
+  }
+
+  checkServiceName() {
+    // if there is no servicename than redirect to home
+    if (this.serviceName == null) {
+      this.router.navigate(['/home']);
+    }
+
+    if (this.serviceName?.toLowerCase() == 'software') {
+      this.isSoftware = true;
+    } else if (this.serviceName?.toLowerCase() == 'website') {
+      this.isWebsite = true;
+    } else if (this.serviceName?.toLowerCase() == 'consulting') {
+      this.isConsulting = true;
+    } else {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  openService(url: string) {
+    if (url == 'software') {
+      this.isSoftware = true;
+      this.isWebsite = false;
+      this.isConsulting = false;
+      this.router.navigate(['/services/software']);
+    } else if (url == 'website') {
+      this.isWebsite = true;
+      this.isSoftware = false;
+      this.isConsulting = false;
+      this.router.navigate(['/services/website']);
+    } else if (url == 'consulting') {
+      this.isConsulting = true;
+      this.isWebsite = false;
+      this.isSoftware = false;
+      this.router.navigate(['/services/consulting']);
+    } else {
+      this.router.navigate(['/home']);
+    }
+  }
 }

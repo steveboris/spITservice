22fddbeae33e6887acf0f6d87003fec893e613e6
steveboris <b.steve189@hotmail.com>
2022-06-13 07:49:16 +0200

    FEA: footer2 implemented

--- src/app/layouts/user-layout/user-layout.routing.ts
+++ src/app/layouts/user-layout/user-layout.routing.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from 'src/app/pages/home/home.component';
+import { ServicesComponent } from 'src/app/pages/services/services.component';
 
 export const UserLayoutRoutes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'services/:name', component: ServicesComponent }
 ];
